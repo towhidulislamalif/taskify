e63e245d1f7918b6e6f188de0caae24f35b46ef5
towhidulislamalif <towhidulislamalif@gmail.com>
2022-12-29 19:05:48 +0600

    some changes

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -75,26 +75,28 @@ const Navbar = () => {
               : 'opacity-0 -translate-x-full absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center'
           }
         >
-          <div className="flex flex-col md:flex-row md:mx-6">
-            <Link
-              className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
-              to="/"
-            >
-              Add Tasks
-            </Link>
-            <Link
-              className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
-              to="/my-tasks"
-            >
-              My Tasks
-            </Link>
-            <Link
-              className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
-              to="/completed-tasks"
-            >
-              Completed Tasks
-            </Link>
-          </div>
+          {user?.uid && (
+            <div className="flex flex-col md:flex-row md:mx-6">
+              <Link
+                className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                to="/"
+              >
+                Add Tasks
+              </Link>
+              <Link
+                className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                to="/my-tasks"
+              >
+                My Tasks
+              </Link>
+              <Link
+                className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                to="/completed-tasks"
+              >
+                Completed Tasks
+              </Link>
+            </div>
+          )}
 
           <div className="flex justify-center md:block gap-4">
             {user?.uid ? (
@@ -105,11 +107,18 @@ const Navbar = () => {
                 Sign out
               </button>
             ) : (
-              <Link to="/sign-in">
-                <button className="px-4 py-2 tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-                  Sign in
-                </button>
-              </Link>
+              <>
+                <Link to="/sign-in">
+                  <button className="px-4 py-2 mr-2 tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                    Sign in
+                  </button>
+                </Link>
+                <Link to="/sign-up">
+                  <button className="px-4 py-2 tracking-wide text-white capitalize transition-colors duration-300 transform bg-teal-600 rounded hover:bg-teal-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                    Sign up
+                  </button>
+                </Link>
+              </>
             )}
           </div>
         </div>
